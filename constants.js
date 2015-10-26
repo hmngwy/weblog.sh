4411b95ce278f77111708d133935af25f4992e91
hmngwy <cp.ambrosio@gmail.com>
2015-10-27 00:56:12 +0800

    updated help

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   mongourl: process.env.MONGO_URL || 'mongodb://localhost/weblogsh',
   endpointurl: process.env.ENDPOINT || 'http://localhost:3000/endpoint',
-  latest: process.env.LATESTCLIENT || 'blog-0.2.1',
+  latest: process.env.LATESTCLIENT || 'blog-0.2.2',
   downloadpath: process.env.DOWNLOADPATH || '/d',
   md5: process.env.MD5 || 'XXXXXXXXXXXXXXXX'
 }
