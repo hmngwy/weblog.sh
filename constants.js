3af607a9011c1b820e4dc4b7191f44321fa102ae
hmngwy <cp.ambrosio@gmail.com>
2015-10-26 21:17:27 +0800

    new client version

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   mongourl: process.env.MONGO_URL || 'mongodb://localhost/weblogsh',
   endpointurl: process.env.ENDPOINT || 'http://localhost:3000/endpoint',
-  latest: process.env.LATESTCLIENT || 'blog-0.1.1',
+  latest: process.env.LATESTCLIENT || 'blog-0.2.0',
   downloadpath: process.env.DOWNLOADPATH || '/d',
   md5: process.env.MD5 || 'XXXXXXXXXXXXXXXX'
 }
